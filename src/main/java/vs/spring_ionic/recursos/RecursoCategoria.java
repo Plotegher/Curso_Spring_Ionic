e8b8a98034f95715257f46b7c3f450fe3663ceac
Plotegher <campinhos.ads@gmail.com>
2025-07-09 17:06:16 -0300

    Teste da primeira classe de domínio - Categoria

--- src/main/java/vs/spring_ionic/recursos/RecursoCategoria.java
+++ src/main/java/vs/spring_ionic/recursos/RecursoCategoria.java
@@ -3,14 +3,25 @@ package vs.spring_ionic.recursos;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import vs.spring_ionic.entidades.Categoria;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class RecursoCategoria
 {
    @RequestMapping(method = RequestMethod.GET)
-   public String listar()
+   public List<Categoria> listar()
    {
-      return "REST funcionando";
+      Categoria cat1 = new Categoria(1, "Informática");
+      Categoria cat2 = new Categoria(2, "Escritório");
+
+      List<Categoria> lista = new ArrayList<>();
+      lista.add(cat1);
+      lista.add(cat2);
+
+      return lista;
    }
 }
