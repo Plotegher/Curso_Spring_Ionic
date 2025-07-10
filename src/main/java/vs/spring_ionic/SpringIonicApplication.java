34ede4fbf4f31c538004b4b1448a5b6e1ca6d234
Plotegher <campinhos.ads@gmail.com>
2025-07-10 09:41:57 -0300

    Criado operação de instanciação de categorias

--- src/main/java/vs/spring_ionic/SpringIonicApplication.java
+++ src/main/java/vs/spring_ionic/SpringIonicApplication.java
@@ -1,13 +1,31 @@
 package vs.spring_ionic;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import vs.spring_ionic.entidades.Categoria;
+import vs.spring_ionic.repositorios.RepositorioCategoria;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-public class SpringIonicApplication {
+public class SpringIonicApplication implements CommandLineRunner
+{
+	@Autowired
+	private RepositorioCategoria repositorioCategoria;
 
 	public static void main(String[] args)
 	{
 		SpringApplication.run(SpringIonicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception
+	{
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		repositorioCategoria.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
